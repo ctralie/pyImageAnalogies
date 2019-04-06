1afd03e14041f0f76eb1c8de148bf0bf0e23d4b5
Chris Tralie <chris.tralie@gmail.com>
2019-04-06 10:17:37 -0400

    Starting to gather the code I wrote before

---
+++ ImageAnalogies.py
@@ -0,0 +1,285 @@
+"""
+Programmer: Chris Tralie
+Purpose: To implement the basic features of Image Analogies[1] in Python,
+using pyflann
+
+Image Analogies
+A. Hertzmann, C. Jacobs, N. Oliver, B. Curless, D. Salesin.
+SIGGRAPH 2001 Conference Proceedings.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as sio
+import scipy.misc
+import pyflann
+from skimage.transform import pyramid_gaussian
+
+def rgb2gray(rgb):
+    """
+    Convert an RGB image to grayscale
+    Parameters
+    ----------
+    rgb: ndarray(M, N, 3)
+        An M x N color image
+    Returns
+    -------
+    gray: ndarray(M, N)
+        The corresponding grayscale image
+    """
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+    return gray
+
+def readImage(filename):
+    """
+    Load an image from disk
+    Parameters
+    ----------
+    filename: string
+        Location of image
+    Returns
+    -------
+    I: ndarray(M, N, 3, dtype=float)
+        A color channel image converted to floating
+        point [0, 1] per channel
+    """
+    I = scipy.misc.imread(filename)
+    I = np.array(I, dtype=np.float32)/255.0
+    return I
+
+def writeImage(I, filename):
+    """
+    Write a floating point image to disk after rounding
+    to the nearest byte for each color channel
+    Parameters
+    ----------
+    I: ndarray(M, N, 3, dtype=float)
+        MxN color image in floating point [0, 1] per channel
+    filename: string
+        The path to which to save the file
+    """
+    IRet = I*255.0
+    IRet[IRet > 255] = 255
+    IRet[IRet < 0] = 0
+    IRet = np.array(IRet, dtype=np.uint8)
+    scipy.misc.imsave(filename, IRet)
+
+def getPatches(I, dim):
+    """
+    Given an an MxN single channel image I, get all dimxdim dimensional
+    patches
+    Parameters
+    ----------
+    I: ndarray(M, N)
+        Single channel image
+    dim: int
+        Dimension of patches
+    Returns
+    -------
+    P: ndarray((M-dim+1), (N-dim+1), dimxdim))
+        Array of flattened patches
+    """
+    #http://stackoverflow.com/questions/13682604/slicing-a-numpy-image-array-into-blocks
+    shape = np.array(I.shape*2)
+    strides = np.array(I.strides*2)
+    W = np.asarray(dim)
+    shape[I.ndim:] = W
+    shape[:I.ndim] -= W - 1
+    if np.any(shape < 1):
+        raise ValueError('Window size %i is too large for image'%dim)
+    P = np.lib.stride_tricks.as_strided(I, shape=shape, strides=strides)
+    P = np.reshape(P, [P.shape[0], P.shape[1], dim*dim])
+    return P
+
+def getCausalPatches(I, dim):
+    """
+    Assuming dim is odd, return L-shaped patches that would
+    occur in raster order
+    Parameters
+    ----------
+    I: ndarray(M, N)
+        Single channel image
+    dim: int
+        Dimension of patches
+    """
+    P = getPatches(I, dim)
+    k = int((dim*dim-1)/2)
+    P = P[:, :, 0:k]
+    return P
+
+def getCoherenceMatch(X, x0, BpLidx, dim, i, j):
+    """
+    Return the best coherence match
+    Parameters
+    ----------
+    X: ndarray(M, N, DimFeatures)
+        Array of feature vectors at each pixel
+    x0: ndarray(DimFeatures)
+        The feature vector of the pixel that's being filled in
+    BpLidx: ndarray(MxN)
+        An MxN array of raveled indices from which pixels have
+        been drawn so far
+    dim: int
+        Dimension of patch
+    i: int
+        Row of pixel
+    j: int
+        Column of pixel
+    Returns
+    -------
+    idxMin: int
+        Index into the array of patches of the closest coherent patch
+    minDistSqr: float   
+        Squared distance of the closest patch
+    """
+    k = int((dim*dim-1)/2)
+    M = X.shape[0]
+    N = X.shape[1]
+    minDistSqr = np.inf
+    idxmin = [-1, -1]
+    [dJ, dI] = np.meshgrid(np.arange(dim), np.arange(dim))
+    dI = np.array(dI.flatten()[0:k], dtype = np.int64) - (dim-1)/2
+    dJ = np.array(dJ.flatten()[0:k], dtype = np.int64) - (dim-1)/2
+    #TODO: Vectorize code below
+    for n in range(dI.size):
+        #Indices of pixel picked for neighbor
+        ni = BpLidx[dI[n]+i, dJ[n]+j][0]
+        nj = BpLidx[dI[n]+i, dJ[n]+j][1]
+        if ni == -1 or nj == -1:
+            continue
+        ni = int(ni - dI[n])
+        nj = int(nj - dJ[n])
+        if ni < 0 or nj < 0 or ni >= M or nj >= N:
+            continue
+        x = X[ni, nj, :]
+        distSqr = np.sum((x - x0)**2)
+        if distSqr < minDistSqr:
+            minDistSqr = distSqr
+            idxmin = [ni, nj]
+    return (idxmin, minDistSqr)
+
+
+def doImageAnalogies(A, Ap, B, Kappa = 0.0, NLevels = 3, KCoarse = 5, KFine = 5):
+    """
+    Perform image analogies
+    Parameters
+    ----------
+    A: ndarray(M, N, 3)
+        A floating point 3-channel image to use as A
+    Ap: ndarray(M, N, 3)
+        A floating point 3-channel image to use as A'
+    B: ndarray(M, N, 3)
+        A floating point 3-channel image to use as B
+    Kappa: float
+        The coherence factor (how much to promote nearby patches
+        over good matches)
+    NLevels: int
+        The number of levels to use in a multiresolution reconstruction
+    KCoarse: int
+        Dimension of square patches to sample at the coarsest level
+        of the multiresolution pyramid
+    KFine: int
+        Dimension of square patches to sample at the finest level
+        of the multiresolution pyramid
+    """
+    #Make image pyramids
+    AL = tuple(pyramid_gaussian(A, NLevels, downscale = 2))
+    ApL = tuple(pyramid_gaussian(Ap, NLevels, downscale = 2))
+    BL = tuple(pyramid_gaussian(B, NLevels, downscale = 2))
+    BpL = []
+    BpLidx = []
+    print("BL:")
+    for i in range(len(BL)):
+        print(BL[i].shape)
+        BpL.append(np.zeros(BL[i].shape))
+        BpLidx.append(-1*np.ones((BL[i].shape[0], BL[i].shape[1], 2)))
+    print("AL:")
+    for i in range(len(AL)):
+        print(AL[i].shape)
+
+    #Do multiresolution synthesis
+    for level in range(NLevels, -1, -1):
+        KSpatial = KFine
+        if level == 0:
+            KSpatial = KCoarse
+        #Step 1: Make features
+        APatches = getPatches(rgb2gray(AL[level]), KSpatial)
+        ApPatches = getCausalPatches(rgb2gray(ApL[level]), KSpatial)
+        X = np.concatenate((APatches, ApPatches), 2)
+        print("X.shape = ", X.shape)
+        B2 = None
+        Bp2 = None
+        if level < NLevels:
+            #Use multiresolution features
+            A2 = scipy.misc.imresize(AL[level+1], AL[level].shape)
+            Ap2 = scipy.misc.imresize(ApL[level+1], ApL[level].shape)
+            A2Patches = getPatches(rgb2gray(A2), KSpatial)
+            Ap2Patches = getPatches(rgb2gray(Ap2), KSpatial)
+            X = np.concatenate((X, A2Patches, Ap2Patches), 2)
+            B2 = scipy.misc.imresize(BL[level+1], BL[level].shape)
+            Bp2 = scipy.misc.imresize(BpL[level+1], BpL[level].shape)
+        annList = pyflann.FLANN()
+        annList.build_index(np.reshape(X, [X.shape[0]*X.shape[1], X.shape[2]]))
+
+        #Step 2: Fill in the first few scanLines to prevent the image
+        #from getting crap in the beginning
+        if level == NLevels:
+            I = np.array(ApL[level]*255, dtype = np.uint8)
+            I = scipy.misc.imresize(I, BpL[level].shape)
+            BpL[level] = np.array(I/255.0, dtype = np.float64)
+        else:
+            I = np.array(BpL[level+1]*255, dtype = np.uint8)
+            I = scipy.misc.imresize(I, BpL[level].shape)
+            BpL[level] = np.array(I/255.0, dtype = np.float64)
+
+        #Step 3: Fill in the pixels in scanline order
+        d = int((KSpatial-1)/2)
+        for i in range(d, BpL[level].shape[0]-d):
+            print(i)
+            for j in range(d, BpL[level].shape[1]-d):
+                #Make the feature at this pixel
+                #Full patch B
+                BPatch = rgb2gray(BL[level][i-d:i+d+1, j-d:j+d+1, :])
+                #Causal patch B'
+                BpPatch = rgb2gray(BpL[level][i-d:i+d+1, j-d:j+d+1, :]).flatten()
+                BpPatch = BpPatch[0:int((KSpatial*KSpatial-1)/2)]
+                F = np.concatenate((BPatch.flatten(), BpPatch.flatten()))
+
+                if level < NLevels:
+                    #Use multiresolution features
+                    BPatch = rgb2gray(B2[i-d:i+d+1, j-d:j+d+1, :])
+                    BpPatch = rgb2gray(Bp2[i-d:i+d+1, j-d:j+d+1, :])
+                    F = np.concatenate((F, BPatch.flatten(), BpPatch.flatten()))
+                #Find index of most closely matching feature point in A
+                #DistSqrFn = XSqr + np.sum(F**2) - 2*X.dot(F)
+                idx = annList.nn_index(F)[0].flatten()
+                idx = np.unravel_index(idx, (X.shape[0], X.shape[1]))
+                if Kappa > 0:
+                #Compare with coherent pixel
+                    (idxc, distSqrc) = getCoherenceMatch(X, F, BpLidx[level], KSpatial, i, j)
+                    distSqr = np.sum((X[idx[0], idx[1]] - F)**2)
+                    fac = 1 + Kappa*(2.0**(level - NLevels))
+                    if distSqrc < distSqr*fac*fac:
+                        idx = idxc
+                BpLidx[level][i, j, :] = idx
+                BpL[level][i, j, :] = ApL[level][idx[0]+d, idx[1]+d, :]
+            if i%20 == 0:
+                writeImage(BpL[level], "%i.png"%level)
+        plt.subplot(122)
+        plt.imshow(BpLidx[level][:, :, 0], cmap = 'Spectral')
+        plt.title("Y")
+        plt.subplot(121)
+        plt.imshow(BpLidx[level][:, :, 1], cmap = 'Spectral')
+        plt.title("X")
+        plt.savefig("%i_idx.png"%level, bbox_inches = 'tight')
+    return BpL[0]
+
+def testCyclops():
+    A = readImage("images/me-mask.png")
+    Ap = readImage("images/me.jpg")
+    B = readImage("images/cyclopsmask.png")
+    res = doImageAnalogies(A, Ap, B, Kappa = 0.1, NLevels = 2)
+
+
+if __name__ == '__main__':
+    testCyclops()
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # pyImageAnalogies
+
+A Python implementation of the basic features Image Analogies technique for style transfer ([1]), which is an incredibly simple but effective classical technique which is an alternative to complicated deep learning approaches if the example image pair is in perfect correspondence.  Features in this implementation include multiresolution pyramids and coherence search weighted against approximate nearest neighbor search over all patches.
+
+* ([1]) A. Hertzmann, C. Jacobs, N. Oliver, B. Curless, D. Salesin. ``Image Analogies.''  SIGGRAPH 2001 Conference Proceedings.
+
+
+## Dependencies
+* Numpy/Scipy/Matplotlib
+* scikit-image
+
+
+## Running
+To see all options, run the script as follows
+~~~~~ bash
+python ImageAnalogies.py --help
+~~~~~
+
+
+
+[1]: <https://mrl.nyu.edu/projects/image-analogies/>
