21e92a124a44af106191d46ace6fb6e98d9b2c2c
Chris Tralie <chris.tralie@gmail.com>
2025-06-02 10:23:00 -0400

    Handling channels better

--- ImageAnalogies.py
+++ ImageAnalogies.py
@@ -175,12 +175,12 @@ def doImageAnalogies(A, Ap, B, Kappa = 0.0, NLevels = 3, KCoarse = 5, KFine = 5,
     Perform image analogies
     Parameters
     ----------
-    A: ndarray(M, N, 3)
-        A floating point 3-channel image to use as A
+    A: ndarray(M, N, ...)
+        A floating point image to use as A, with an optional channel dimension
     Ap: ndarray(M, N, 3)
-        A floating point 3-channel image to use as A'
+        A floating point image to use as A', with an optional channel dimension
     B: ndarray(M, N, 3)
-        A floating point 3-channel image to use as B
+        A floating point image to use as B, with an optional channel dimension
     Kappa: float
         The coherence factor (how much to promote nearby patches
         over good matches)
@@ -197,19 +197,33 @@ def doImageAnalogies(A, Ap, B, Kappa = 0.0, NLevels = 3, KCoarse = 5, KFine = 5,
     
     Returns
     -------
-    Bp: ndarray(M, N, 3)
-        A floating point 3-channel image of the synthesized image analogy
+    Bp: ndarray(M, N, ...)
+        A floating point image of the synthesized image analogy, with the corresponding
+        channel dimension
     """
     #Make image pyramids
-    AL = tuple(pyramid_gaussian(A, NLevels, downscale = 2, channel_axis=-1))
-    ApL = tuple(pyramid_gaussian(Ap, NLevels, downscale = 2, channel_axis=-1))
-    BL = tuple(pyramid_gaussian(B, NLevels, downscale = 2, channel_axis=-1))
-    BpL = [] # B' values in pyramid
+    channel_axis = {}
+    if len(A.shape) > 2:
+        channel_axis = {"channel_axis":-1} # pyramic_gaussian needs this parameter for color images
+    AL = tuple(pyramid_gaussian(A, NLevels, downscale = 2, **channel_axis))
+    ApL = tuple(pyramid_gaussian(Ap, NLevels, downscale = 2, **channel_axis))
+    BL = tuple(pyramid_gaussian(B, NLevels, downscale = 2, **channel_axis))
+    ApLColor = [img for img in ApL]
+    if len(A.shape) > 2: # For color images, use grayscale features, as recommended in the paper
+        AL = [rgb2gray(img) for img in AL]
+        ApL = [rgb2gray(img) for img in ApL]
+        BL = [rgb2gray(img) for img in BL]
+    BpL = [] # B' values in pyramid (grayscale)
+    BpLColor = [] # B' values in pyramid (color)
     BpLidx = [] # Indices of nearest neighbors at each pixel in pyramid
     print("BL:")
     for i in range(len(BL)):
         print(BL[i].shape)
         BpL.append(np.zeros(BL[i].shape))
+        shape = list(BL[i].shape)
+        if len(A.shape) > 2:
+            shape.append(A.shape[2])
+        BpLColor.append(np.zeros(shape))
         BpLidx.append(-1*np.ones((BL[i].shape[0], BL[i].shape[1], 2), dtype=int))
     print("AL:")
     for i in range(len(AL)):
@@ -222,8 +236,8 @@ def doImageAnalogies(A, Ap, B, Kappa = 0.0, NLevels = 3, KCoarse = 5, KFine = 5,
         if level == NLevels:
             KSpatial = KCoarse
         #Step 1: Make features
-        APatches = get_patches(rgb2gray(AL[level]), KSpatial)
-        ApPatches = get_causal_patches(rgb2gray(ApL[level]), KSpatial)
+        APatches = get_patches(AL[level], KSpatial)
+        ApPatches = get_causal_patches(ApL[level], KSpatial)
         X = np.concatenate((APatches, ApPatches), 2)
         B2 = None
         Bp2 = None
@@ -231,8 +245,8 @@ def doImageAnalogies(A, Ap, B, Kappa = 0.0, NLevels = 3, KCoarse = 5, KFine = 5,
             #Use multiresolution features
             A2 = imresize(AL[level+1], AL[level].shape)
             Ap2 = imresize(ApL[level+1], ApL[level].shape)
-            A2Patches = get_patches(rgb2gray(A2), KSpatial)
-            Ap2Patches = get_patches(rgb2gray(Ap2), KSpatial)
+            A2Patches = get_patches(A2, KSpatial)
+            Ap2Patches = get_patches(Ap2, KSpatial)
             X = np.concatenate((X, A2Patches, Ap2Patches), 2)
             B2 = imresize(BL[level+1], BL[level].shape)
             Bp2 = imresize(BpL[level+1], BpL[level].shape)
@@ -251,20 +265,21 @@ def doImageAnalogies(A, Ap, B, Kappa = 0.0, NLevels = 3, KCoarse = 5, KFine = 5,
 
         #Step 3: Fill in the pixels in scanline order
         d = int((KSpatial-1)/2)
+        #sz = KSpatial*KSpatial + KSpatial*(KSpatial-1)//2  #B, Bp
         for i in tqdm(range(d, BpL[level].shape[0]-d)):
             for j in range(d, BpL[level].shape[1]-d):
                 #Make the feature at this pixel
                 #Full patch B
-                BPatch = rgb2gray(BL[level][i-d:i+d+1, j-d:j+d+1, :])
+                BPatch = BL[level][i-d:i+d+1, j-d:j+d+1]
                 #Causal patch B'
-                BpPatch = rgb2gray(BpL[level][i-d:i+d+1, j-d:j+d+1, :]).flatten()
+                BpPatch = BpL[level][i-d:i+d+1, j-d:j+d+1].flatten()
                 BpPatch = BpPatch[0:int((KSpatial*KSpatial-1)/2)]
                 F = np.concatenate((BPatch.flatten(), BpPatch.flatten()))
 
                 if level < NLevels:
                     #Use multiresolution features
-                    BPatch = rgb2gray(B2[i-d:i+d+1, j-d:j+d+1, :])
-                    BpPatch = rgb2gray(Bp2[i-d:i+d+1, j-d:j+d+1, :])
+                    BPatch = B2[i-d:i+d+1, j-d:j+d+1]
+                    BpPatch = Bp2[i-d:i+d+1, j-d:j+d+1]
                     F = np.concatenate((F, BPatch.flatten(), BpPatch.flatten()))
                 #Find index of most closely matching feature point in A
                 dist, idx = nn.kneighbors(F[None, :])
@@ -278,9 +293,10 @@ def doImageAnalogies(A, Ap, B, Kappa = 0.0, NLevels = 3, KCoarse = 5, KFine = 5,
                     if distSqrc < distSqr*fac*fac:
                         idx = idxc
                 BpLidx[level][i, j, :] = idx
-                BpL[level][i, j, :] = ApL[level][idx[0]+d, idx[1]+d, :]
+                BpL[level][i, j] = ApL[level][idx[0]+d, idx[1]+d]
+                BpLColor[level][i, j, ...] = ApLColor[level][idx[0]+d, idx[1]+d, :]
             if i%20 == 0 and debug_images:
-                write_image(BpL[level], "%i.png"%level)
+                write_image(BpLColor[level], "%i.png"%level)
         if debug_images:
             plt.subplot(122)
             plt.imshow(BpLidx[level][:, :, 0], cmap = 'Spectral')
@@ -289,7 +305,7 @@ def doImageAnalogies(A, Ap, B, Kappa = 0.0, NLevels = 3, KCoarse = 5, KFine = 5,
             plt.imshow(BpLidx[level][:, :, 1], cmap = 'Spectral')
             plt.title("X")
             plt.savefig("%i_idx.png"%level, bbox_inches = 'tight')
-    return BpL[0]
+    return BpLColor[0]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
